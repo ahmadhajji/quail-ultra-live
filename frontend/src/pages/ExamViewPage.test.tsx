b69a6e03ad4fbdd5b7220bb305902d175f2c0887
Ahmad <ahmad2003.hajji@gmail.com>
2026-04-01 00:52:23 +0400

    Restore exam styling and exit after finishing blocks

--- frontend/src/pages/ExamViewPage.test.tsx
+++ frontend/src/pages/ExamViewPage.test.tsx
@@ -62,7 +62,8 @@ describe('ExamViewPage', () => {
       expect(htmlHelpers.fetchQuestionAssets).toHaveBeenCalled()
     })
 
-    await userEvent.click(screen.getByRole('button', { name: /alpha/i }))
+    const alphaChoice = screen.getAllByRole('button', { name: /alpha/i })[0]!
+    await userEvent.click(alphaChoice)
     await userEvent.click(screen.getByRole('button', { name: 'Submit Answer' }))
 
     expect(await screen.findByText('Explanation')).toBeInTheDocument()
